901868091b19eb1e6fbe3962fb30fa5ae3d360f3
doraemon49 <min1593@nate.com>
2025-04-01 00:14:15 +0900

    [FEAT]:Dockerfile,AWS RDS연동,AWS EC2배포

--- app/models/database.py
+++ app/models/database.py
@@ -4,9 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 로컬 SQLite DB (나중에 RDS로 변경 가능)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
-# 예: RDS PostgreSQL: "postgresql://username:password@host:port/dbname"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+from dotenv import load_dotenv
+import os
+load_dotenv()  # .env 파일 읽기
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# sqlite 사용시
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/models/models.py
+++ app/models/models.py
@@ -7,8 +7,8 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     userInfoAgree = Column(Boolean, nullable=False)     # 개인정보 동의 (1: 동의, 0: 비동의)
-    userName = Column(String, nullable=False)
+    userName = Column(String(100), nullable=False)
     userDateOfBirth = Column(Date, nullable=False)
-    userGender = Column(Boolean, nullable=False)        # 성별  (true: 남성, false: 여성)
-    userEdu = Column(String, nullable=False)            # 학력
-    userPreResult = Column(String, nullable=False)      # 기존 인지 기능 점검 결과(”없음”, “정상”, “SCI” 등 - 미정)
+    userGender = Column(String(100), nullable=False)        # 성별  (true: 남성, false: 여성)
+    userEdu = Column(String(100), nullable=False)            # 학력
+    userPreResult = Column(String(100), nullable=False)      # 기존 인지 기능 점검 결과(”없음”, “정상”, “SCI” 등 - 미정)
